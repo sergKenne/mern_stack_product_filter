df3f2a4d902b89f9cea32caea3e67fe8da1167a7
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-04-26 14:14:29 +0300

    make deployement

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -33,7 +33,7 @@ const Products = () => {
                       <div className="col-md-3 px-2" key={prod._id}>
                           <div className="card">
                               <img
-                                  src={`http://localhost:3000/${prod.image}`}
+                                  src={`/uploads/${prod.image}`}
                                   className="card-img-top img-max-width "
                                   alt="..."
                               />
