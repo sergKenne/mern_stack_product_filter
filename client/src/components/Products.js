9fa1ecc6bd7d1b1f142b0f7a9acb1f89e8f672ba
Serge Simplice Kenne <kenne_serge@mail.ru>
2023-07-03 17:41:31 +0300

    add some style

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect} from 'react'
 import { productsContext, PRODUCTS_LIST_REQUEST, PRODUCTS_LIST_SUCCESS } from '../context/ContextProvider';
 import axios from 'axios'
 import Loading from './Loading';
+import { BASE_URL } from '../utils/base_url';
 
 const Products = () => {
     
@@ -12,7 +13,8 @@ const Products = () => {
         const fetchProducts = async () => {
             dispatch({ type: PRODUCTS_LIST_REQUEST })
             try {
-                const { data } = await axios.get('/api/product');
+                //const { data } = await axios.get('/api/product');
+                const { data } = await axios.get(`${BASE_URL}/api/product`); //for deployement
                 dispatch({ type: PRODUCTS_LIST_SUCCESS, products: data })
             } catch (err) {
                 dispatch({type: err.message})
@@ -30,10 +32,11 @@ const Products = () => {
               <div className="row filter_data">
                   {products.length === 0 && <h1> No Data Found</h1>}
                   {products.map((prod) => (
-                      <div className="col-md-3 px-2" key={prod._id}>
+                      <div className="col-sm-4 col-md-3 px-2" key={prod._id}>
                           <div className="card">
                               <img
-                                  src={`http://localhost:3000/${prod.image}`}
+                                  //src={`http://localhost:3000/${prod.image}`}
+                                  src={`${BASE_URL}/${prod.image}`}
                                   className="card-img-top img-max-width "
                                   alt="..."
                               />
@@ -42,10 +45,18 @@ const Products = () => {
                                       <strong>{prod.name}</strong>
                                   </h5>
                                   <p className="card-text card-text--price">{prod.price}</p>
-                                  <p className="card-text">Camera: <span>{prod.camera}MP</span> </p>
-                                  <p className="card-text">Brand: <span>{prod.brand}</span></p>
-                                  <p className="card-text">RAM: <span>{prod.ram}GB</span></p>
-                                  <p className="card-text">Storage: <span>{prod.storage}GB</span></p>
+                                  <p className="card-text">
+                                      Camera: <span>{prod.camera}MP</span>{' '}
+                                  </p>
+                                  <p className="card-text">
+                                      Brand: <span>{prod.brand}</span>
+                                  </p>
+                                  <p className="card-text">
+                                      RAM: <span>{prod.ram}GB</span>
+                                  </p>
+                                  <p className="card-text">
+                                      Storage: <span>{prod.storage}GB</span>
+                                  </p>
                               </div>
                           </div>
                       </div>
---
+++ client/src/utils/base_url.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://mern-stack-product-filter.onrender.com';
